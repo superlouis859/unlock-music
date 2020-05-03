adb95592515d3eb6a68bd9ef407f774b128e9b00
superlouis859 <64701375+superlouis859@users.noreply.github.com>
2020-05-03 17:04:30 +0800

    Add files via upload

---
+++ precache-manifest.6ed3441a01bcc627240b5f989c02cee8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a9fe618d3537e44f730e50f9c34f39",
+    "url": "2fe81c5d831b703a81e1.worker.js"
+  },
+  {
+    "revision": "4ab915550563b717adb3",
+    "url": "css/app.2a97a82f.css"
+  },
+  {
+    "revision": "433de2ece7f4afc95cca",
+    "url": "css/chunk-vendors.8cf7dd44.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "11a7f8e9affddf72b74fbab3ae0017a7",
+    "url": "index.html"
+  },
+  {
+    "revision": "4ab915550563b717adb3",
+    "url": "js/app.cb7e65d9.js"
+  },
+  {
+    "revision": "433de2ece7f4afc95cca",
+    "url": "js/chunk-vendors.909f65e9.js"
+  },
+  {
+    "revision": "28f3e13ec88073aa1b85b7d66358f613",
+    "url": "manifest.json"
+  }
+]);
---
+++ precache-manifest.79ac192429ff8d2cc35e6f455e92a061.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3757dfa20bc8bd28a49f29b0681e49d",
+    "url": "892728b921075d001370.worker.js"
+  },
+  {
+    "revision": "03b1c12e932eaceaba54",
+    "url": "css/app.2a97a82f.css"
+  },
+  {
+    "revision": "745f2b1a1d1b62ddd540",
+    "url": "css/chunk-vendors.8cf7dd44.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "0570fe76161f720fdcb74ce6139bc6ca",
+    "url": "index.html"
+  },
+  {
+    "revision": "03b1c12e932eaceaba54",
+    "url": "js/app-legacy.bee358f1.js"
+  },
+  {
+    "revision": "745f2b1a1d1b62ddd540",
+    "url": "js/chunk-vendors-legacy.09a40896.js"
+  },
+  {
+    "revision": "28f3e13ec88073aa1b85b7d66358f613",
+    "url": "manifest.json"
+  }
+]);
